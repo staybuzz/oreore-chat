87c92defe4640a7c5fda6e219149a37c983934ec
staybuzz <staybuzz.k@gmail.com>
2013-06-14 19:06:02 +0900

    Sequel実装途中

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source "https://rubygems.org"
 
 # gem "rails"
 gem "sinatra"
+gem "sinatra-contrib"
 gem "haml"
 gem "sequel"
 gem "sqlite3"
--- oreore.rb
+++ oreore.rb
@@ -1,15 +1,36 @@
 require "sinatra"
+require 'sinatra/reloader'
 require 'haml'
 require 'sequel'
 require 'sqlite3'
 
+# DB = Sequel.sqlite('db/chat.db')
+
+#DB.create_table :items do
+#  primary_key :id
+#  String :name
+#  String :text
+#end
+
+Sequel.sqlite('db/chat.db')
+class Entries < Sequel::Model
+  unless table_exists?
+    set_schema do
+      primary_key :id
+      string :name
+      string :text
+    end
+    create_table
+  end
+end
 
 get '/' do
-  'Hello World!'
+  @entries = Entries.all
   haml :index
 end
 
 post '/add' do
   @str = params[:str]
-  haml :index
+  Entries.insert(:text => @str)
+  redirect '/'
 end
